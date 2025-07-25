917c17302400d99a5a9f27219eed0c181f5c915d
jesaja44 <jesaja-ro@web.de>
2025-07-25 12:11:46 +0200

    test

---
+++ infrastructure/main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+data "aws_vpc" "selected" {
+  id = "vpc-09d707260f1cee0ed"
+}
+
+data "aws_subnets" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = "ec2-sg-"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "grocery-ec2-sg"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds-sg-"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "grocery-rds-sg"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0394cacf99ecccb4d" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id              = data.aws_subnets.selected.ids[0]
+
+  # IAM Role anhängen (hier ggf. anpassen)
+  # iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+
+  tags = {
+    Name = "grocery-ec2"
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "grocery-db-subnet-group"
+  subnet_ids = data.aws_subnets.selected.ids
+
+  tags = {
+    Name = "Grocery DB Subnet Group"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier             = "mario-rose-db"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15"
+  instance_class         = "db.t3.micro"
+  username               = var.db_username
+  password               = var.db_password
+  db_name                = var.db_name
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+
+  tags = {
+    Name = "grocery-rds"
+  }
+}
+#test
+resource "aws_s3_bucket" "avatars" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
