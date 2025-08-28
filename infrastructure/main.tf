c14fa94b3568760c5751398d45c17ab7d445ef67
jesaja44 <jesaja-ro@web.de>
2025-08-28 13:54:19 +0200

    infra(sg): remove inline SSH ingress; keep single source via aws_vpc_security_group_ingress_rule

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,13 +20,6 @@ data "aws_subnets" "selected" {
 resource "aws_security_group" "ec2_sg" {
   name_prefix = "ec2-sg-"
   vpc_id      = data.aws_vpc.selected.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["2.213.63.38/32"]
-  }
   egress {
     from_port   = 0
     to_port     = 0
